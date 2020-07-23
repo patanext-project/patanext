d57c062b8a0f5acce3937b39879401ee622a9198
Guerro323 <kaltobattle@gmail.com>
2020-07-23 12:46:28 +0200

    added resource keys

---
+++ PataNext.Simulation.Mixed/Resources/Keys/EquipmentResourceKey.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using GameHost.Native;
+using GameHost.Simulation.Features.ShareWorldState.BaseSystems;
+using GameHost.Simulation.Utility.Resource.Components;
+
+namespace PataNext.Module.Simulation.Resources.Keys
+{
+	public readonly struct EquipmentResourceKey : IGameResourceKeyDescription, IEquatable<EquipmentResourceKey>
+	{
+		public readonly CharBuffer64 Value;
+
+		public class Register : RegisterGameHostComponentData<GameResourceKey<EquipmentResourceKey>>
+		{
+		}
+
+		public bool Equals(EquipmentResourceKey other)
+		{
+			return Value.Equals(other.Value);
+		}
+
+		public override bool Equals(object obj)
+		{
+			return obj is EquipmentResourceKey other && Equals(other);
+		}
+
+		public override int GetHashCode()
+		{
+			return Value.GetHashCode();
+		}
+	}
+}
---
+++ PataNext.Simulation.Mixed/Resources/Keys/UnitKitResourceKey.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using GameHost.Native;
+using GameHost.Simulation.Features.ShareWorldState.BaseSystems;
+using GameHost.Simulation.Utility.Resource.Components;
+
+namespace PataNext.Module.Simulation.Resources.Keys
+{
+	public readonly struct UnitKitResourceKey : IGameResourceKeyDescription, IEquatable<UnitKitResourceKey>
+	{
+		public readonly CharBuffer64 Value;
+
+		public class Register : RegisterGameHostComponentData<GameResourceKey<UnitKitResourceKey>>
+		{
+		}
+
+		public bool Equals(UnitKitResourceKey other)
+		{
+			return Value.Equals(other.Value);
+		}
+
+		public override bool Equals(object obj)
+		{
+			return obj is UnitKitResourceKey other && Equals(other);
+		}
+
+		public override int GetHashCode()
+		{
+			return Value.GetHashCode();
+		}
+	}
+}
