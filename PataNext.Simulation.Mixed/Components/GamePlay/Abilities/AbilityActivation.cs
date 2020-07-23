9cdf6ea26f93d930ff283d9d25cb5111b7f6da74
Guerro323 <kaltobattle@gmail.com>
2020-07-23 18:20:50 +0200

    Create AbilityActivation.cs

---
+++ PataNext.Simulation.Mixed/Components/GamePlay/Abilities/AbilityActivation.cs
@@ -0,0 +1,42 @@
+﻿using GameHost.Native;
+using GameHost.Simulation.Features.ShareWorldState.BaseSystems;
+using GameHost.Simulation.TabEcs;
+using GameHost.Simulation.TabEcs.Interfaces;
+
+namespace PataNext.Module.Simulation.Components.GamePlay.Abilities
+{
+	public enum EAbilityActivationType
+	{
+		Normal,
+		HeroMode,
+		Custom
+	}
+
+	public struct AbilityActivation : IComponentData
+	{
+		public EAbilityActivationType Type;
+		public int             HeroModeMaxCombo;
+		public int             HeroModeImperfectLimitBeforeDeactivation;
+
+		public AbilitySelection Selection;
+
+		/// <summary>
+		/// The command used for chaining.
+		/// </summary>
+		public GameEntity Chaining;
+
+		/// <summary>
+		/// Combo command list, excluding the chaining command.
+		/// </summary>
+		public FixedBuffer32<GameEntity> Combos; //< 32 bytes should suffice, it would be 4 combo commands...
+
+		/// <summary>
+		/// Allowed commands for chaining in hero mode.
+		/// </summary>
+		public FixedBuffer64<GameEntity> HeroModeAllowedCommands; //< 64 bytes should suffice, it would be up to 8 commands...
+		
+		public class Register : RegisterGameHostComponentData<AbilityActivation>
+		{
+		}
+	}
+}
