254a7623550df779ed0a68bee9154c6ee16dc5e6
Guerro323 <kaltobattle@gmail.com>
2020-12-16 23:26:29 +0100

    Only update abilities under authority

--- PataNext.Simulation.Mixed/Game/GamePlay/Abilities/ExecuteActiveAbilitySystem.cs
+++ PataNext.Simulation.Mixed/Game/GamePlay/Abilities/ExecuteActiveAbilitySystem.cs
@@ -7,6 +7,7 @@ using GameHost.Simulation.Utility.EntityQuery;
 using PataNext.Module.Simulation.Components.GamePlay.Abilities;
 using PataNext.Module.Simulation.Passes;
 using PataNext.Simulation.mixed.Components.GamePlay.Abilities;
+using StormiumTeam.GameBase.Network.Authorities;
 using StormiumTeam.GameBase.SystemBase;
 
 namespace PataNext.Module.Simulation.Game.GamePlay.Abilities
@@ -45,7 +46,8 @@ namespace PataNext.Module.Simulation.Game.GamePlay.Abilities
 			var executableAccessor         = GetAccessor<ExecutableAbility>();
 			foreach (var entity in executorQuery ??= CreateEntityQuery(new[]
 			{
-				typeof(OwnerActiveAbility)
+				typeof(OwnerActiveAbility),
+				typeof(SimulationAuthority)
 			}))
 			{
 				ref readonly var ownerActiveAbility = ref ownerActiveAbilityAccessor[entity];
